16376df67f83613e14fe4521dae2c7f1a753d360
Charles Martin Reed <charlesmartinreed@icloud.com>
2019-01-02 23:50:46 -0600

    Add grass and physics body to SKScene.

--- Coin Man/GameScene.swift
+++ Coin Man/GameScene.swift
@@ -8,6 +8,7 @@
 // <div>Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/"                 title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"                 title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
 //<div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/"             title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"             title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
 //<div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/"                 title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"                 title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+//<div>Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/"                 title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"                 title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
 
 import SpriteKit
 import GameplayKit
@@ -16,6 +17,7 @@ class GameScene: SKScene {
     
     //MARK:- Properties
     private var coinMan: SKSpriteNode!
+    private var coinManRunningSpeed: TimeInterval = 0.09
     private var ground: SKSpriteNode!
     private var ceiling: SKSpriteNode!
     
@@ -45,11 +47,11 @@ class GameScene: SKScene {
         }
     }
     
-    var countdown: Int = 3 {
-        didSet {
-            countdownLabel.text = "\(countdown)"
-        }
-    }
+    var countdown: Int = 3
+//        didSet {
+//            countdownLabel.text = "\(countdown)"
+//        }
+    //}
     
     override func didMove(to view: SKView) {
         
@@ -61,6 +63,7 @@ class GameScene: SKScene {
         initializeCoinMan()
         initializeCoinTimer()
         initializeBombTimer()
+        createGrass()
         
     }
     
@@ -75,20 +78,20 @@ class GameScene: SKScene {
         scoreLabel?.position = CGPoint(x: frame.minX * 0.5, y: frame.maxY - 150) //position at the top left
         addChild(scoreLabel)
         
-        ground = SKSpriteNode(color: SKColor.green, size: CGSize(width: self.size.width, height: 140))
-        //ground = childNode(withName: "ground") as? SKSpriteNode
-        ground?.position = CGPoint(x: 0, y: -598.375)
+//        ground = SKSpriteNode(color: SKColor.green, size: CGSize(width: self.size.width, height: 100))
+//        //ground = childNode(withName: "ground") as? SKSpriteNode
+//        ground?.position = CGPoint(x: 0, y: -598.375)
+//
+//        ground.physicsBody = SKPhysicsBody(rectangleOf: ground.size)
+//        ground.physicsBody?.isDynamic = false
+//        ground.physicsBody?.affectedByGravity = false
+//        ground.physicsBody?.categoryBitMask = boundingCategory
+//        ground.physicsBody?.collisionBitMask = coinManCategory
+//        addChild(ground)
         
-        ground.physicsBody = SKPhysicsBody(rectangleOf: ground.size)
-        ground.physicsBody?.isDynamic = false
-        ground.physicsBody?.affectedByGravity = false
-        ground.physicsBody?.categoryBitMask = boundingCategory
-        ground.physicsBody?.collisionBitMask = coinManCategory
-        addChild(ground)
-        
-        ceiling = SKSpriteNode(color: SKColor.blue, size: CGSize(width: self.size.width, height: 75))
+        ceiling = SKSpriteNode(color: SKColor.blue, size: CGSize(width: self.size.width, height: 100))
         //ceiling = childNode(withName: "ceiling") as? SKSpriteNode
-        ceiling.position = CGPoint(x: -0, y: 819.538)
+        ceiling.position = CGPoint(x: -0, y: 719.538)
         
         ceiling.physicsBody = SKPhysicsBody(rectangleOf: ceiling.size)
         ceiling.physicsBody?.isDynamic = false
@@ -103,8 +106,9 @@ class GameScene: SKScene {
         //coinMan = childNode(withName: "coinMan") as? SKSpriteNode
         coinMan = SKSpriteNode(color: SKColor.purple, size: CGSize(width: 100, height: 200))
         
-        guard let groundPosition = ground?.position else { return }
-        coinMan?.position = CGPoint(x: groundPosition.x, y: groundPosition.y + coinMan.size.height)
+        //let startX = (-size.width / 2) + (coinMan.size.width / 2)
+        //let startY = -size.height / 2 + (coinMan.size.height / 2) - 21
+        coinMan?.position = CGPoint(x: 0, y: 0)
         
         addChild(coinMan)
         
@@ -115,6 +119,13 @@ class GameScene: SKScene {
         coinMan.physicsBody?.categoryBitMask = coinManCategory
         coinMan.physicsBody?.contactTestBitMask = coinCategory | bombCategory
         coinMan.physicsBody?.collisionBitMask = boundingCategory //coin man should only be able to "collide" with bounding areas
+        
+        //animate the player sprite
+        var coinManRunning = [SKTexture]()
+        for num in 1...4 {
+            coinManRunning.append(SKTexture(imageNamed: "frame-\(num)"))
+        }
+        coinMan.run(SKAction.repeatForever(SKAction.animate(with: coinManRunning, timePerFrame: coinManRunningSpeed)))
         
         
     }
@@ -191,6 +202,28 @@ class GameScene: SKScene {
         bomb.run(SKAction.sequence(actions))
     }
     
+    func createGrass() {
+        //one piece of grass moves to the left, is removed from the nodes, new grass is created, rinse, wash, repeat
+        let sizingGrass = SKSpriteNode(imageNamed: "grass")
+        let numberOfGrassObjects = Int(size.width / sizingGrass.size.width) + 1
+        
+        for number in 0...numberOfGrassObjects {
+            let grass = SKSpriteNode(imageNamed: "grass")
+            grass.physicsBody = SKPhysicsBody(rectangleOf: grass.size)
+            grass.physicsBody?.isDynamic = false
+            grass.physicsBody?.affectedByGravity = false
+            grass.physicsBody?.categoryBitMask = boundingCategory
+            grass.physicsBody?.collisionBitMask = coinManCategory
+            addChild(grass)
+            
+            //positioning the grass
+            let grassX = (-size.width / 2) + (grass.size.width / 2) + grass.size.width * CGFloat(number)
+            let grassY = -size.height / 2 + (grass.size.height / 2) - 20
+            grass.position = CGPoint(x: grassX, y: grassY)
+        }
+        
+    }
+    
     func checkForOverlappingNodes(node: SKSpriteNode) -> Bool {
         //grab the list of current nodes
         let visibleNodes = self.children
@@ -220,17 +253,20 @@ class GameScene: SKScene {
             yourScoreLabel = SKLabelNode(text: "Your Score:")
             yourScoreLabel.position = CGPoint(x: 0, y: 200)
             yourScoreLabel.fontSize = 100
+            yourScoreLabel.zPosition = 1
             addChild(yourScoreLabel)
         
             finalScoreLabel = SKLabelNode(text: "\(score)")
             finalScoreLabel.position = CGPoint(x: 0, y: 0)
             finalScoreLabel.fontSize = 200
+            finalScoreLabel.zPosition = 1
             addChild(finalScoreLabel)
         
             //create the restart level button
             playButton = SKSpriteNode(imageNamed: "playButton")
             playButton.name = "play"
             playButton.position = CGPoint(x: 0, y: -200)
+            playButton.zPosition = 1
             addChild(playButton)
     }
     
@@ -258,15 +294,12 @@ class GameScene: SKScene {
     }
     
     func startNewRoundCountdown() {
-        startTimer = Timer.scheduledTimer(withTimeInterval: startTimerInterval, repeats: true, block: { [unowned self] (_) in
+        startTimer = Timer.scheduledTimer(withTimeInterval: 3.0, repeats: false, block: { [unowned self] (_) in
             
-            while self.startTimerInterval > 0.0 {
-                self.startTimerInterval -= 1.0
-                //self.countdown -= 1
+            while self.countdown > 0 {
+                self.countdown -= 1
             }
             
-            
-            print(self.startTimerInterval)
             //create a countdown label and display it in the center of the view
 //            self.countdownLabel = SKLabelNode(fontNamed: "AvenirNextCondensed-Bold")
 //            self.countdownLabel.text = "\(self.countdown)"
@@ -275,25 +308,29 @@ class GameScene: SKScene {
 //            self.countdownLabel.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
 //            self.addChild(self.countdownLabel)
             
-            if self.startTimerInterval == 0.0 {
+            if self.countdown == 0 {
                 self.startTimer?.invalidate()
                 //self.countdownLabel.removeFromParent()
                 self.initalizePlayArea()
                 self.initializeCoinMan()
                 self.initializeCoinTimer()
                 self.initializeBombTimer()
+                self.createGrass()
             }
             
             //reset the timer interval
-            self.startTimerInterval = 3.0
+            self.countdown = 3
             
         })
     }
     
     //MARK:- SK methods
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        //make coinman jump
-        coinMan.physicsBody?.applyForce(CGVector(dx: 0, dy: 20_000))
+        
+        //make coinman jump - checking to see if scene is paused to stop vector for growing with each tap
+        if scene?.isPaused == false {
+            coinMan.physicsBody?.applyForce(CGVector(dx: 0, dy: 20_000))
+        }
         
         //check whether the play button was touched
         let touch = touches.first
